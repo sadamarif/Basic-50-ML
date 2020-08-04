3decf5792f6351362f8041910d70e83e96030259
sadamarif <mepcoarif@gmail.com>
2020-08-04 09:11:56 +0530

    Create SelectingElements.py
    Added example for selecting vectors and arrays

---
+++ SelectElements/SelectingElements.py
@@ -0,0 +1,12 @@
+# Load library
+import numpy as np
+# Create row vector
+vector = np.array([1, 2, 3, 4, 5, 6])
+# Create matrix
+matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# Select third element of vector
+vector[2]
+# 3
+# Select second row, second column
+matrix[1,1]
+# 5
