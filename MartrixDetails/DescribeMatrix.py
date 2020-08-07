4df87c35ac2f087832b759f10452ed8365e8e76c
sadamarif <mepcoarif@gmail.com>
2020-08-07 19:58:34 +0530

    Create DescribeMatrix.py
    Ways to identify details of a matrix

---
+++ MartrixDetails/DescribeMatrix.py
@@ -0,0 +1,13 @@
+# Load library
+import numpy as np
+# Create matrix
+matrix = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+# View number of rows and columns
+matrix.shape
+# (3, 4)
+# View number of elements (rows * columns)
+matrix.size
+# 12
+# View number of dimensions
+matrix.ndim
+# 2
