2a674e510a96c1f94197073fa4ef3d2ef95d424d
sadamarif <mepcoarif@gmail.com>
2020-08-09 20:49:03 +0530

    Create RandomValue.py
    Added samples for multiple ways of random number creation

---
+++ RandomValueGenerator/RandomValue.py
@@ -0,0 +1,13 @@
+# Load library
+import numpy as np
+# Set seed
+np.random.seed(0)
+# Generate five random floats between 0.0 and 1.0
+np.random.random(5)
+
+# Generate five random integers between 1 and 10
+np.random.randint(0, 11, 5)
+
+# Draw five numbers from a normal distribution with mean 0.0
+# and standard deviation of 1.0
+np.random.normal(0.0, 1.0, 5)
